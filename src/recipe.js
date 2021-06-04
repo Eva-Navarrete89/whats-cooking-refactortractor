60ed7be32da058e633492de3cc817c88ef6af123
Eva Navarrete <evanavarrete07@gmail.com>
2021-06-04 11:53:33 -0600

    Add test to recipe class

--- src/recipe.js
+++ src/recipe.js
@@ -2,10 +2,13 @@ class Recipe {
   constructor(recipe, ingredientsData) {
     this.name = recipe.name;
     this.id = recipe.id;
+    this.image = recipe.image;
+    this.tags = recipe.tags;
     this.ingredients = recipe.ingredients;
     this.instructions = recipe.instructions;
-    this.tags = recipe.tags;
     this.ingredientsData = ingredientsData;
+    // console.log('jnddkbvldj', this.ingredients);
+    // console.log('yo', recipe);
   }
 
   calculateCost() {
--- test/recipe-test.js
+++ test/recipe-test.js
@@ -1,32 +1,72 @@
-import { expect } from 'chai';
+import {
+  expect
+} from 'chai';
 
 import Recipe from '../src/recipe.js';
 import recipeData from '../src/data/recipes.js';
 import ingredientsData from '../src/data/ingredients.js';
 
-let recipe;
 
 describe('Recipe', () => {
+  let recipe;
   beforeEach(() => {
-
     recipe = new Recipe(recipeData[47], ingredientsData);
   });
 
-  describe('Recipe Data', () => {
+  it('Should be a function', () => {
+    expect(Recipe).to.be.a('function');
+  });
 
-    it('Should hold its own ingredient data', () => {
-      expect(recipe.ingredients).to.equal(recipeData[47].ingredients);
-    })
+  it('should be an instance of recipe', () => {
+    expect(recipe).to.be.instanceof(Recipe);
+  });
+  it('Should have a name', () => {
+    expect(recipe.name).to.equal('Farmer’s Market Flatbread Pizza');
 
-    it('Should hold its own instruction data', () => {
-      expect(recipe.instructions).to.equal(recipeData[47].instructions);
-    })
-  })
+  });
+
+  it('Should have an id', () => {
+    expect(recipe.id).to.equal(601216);
+  });
+
+  it('should have an image', () => {
+    expect(recipe.image).to.equal('https://spoonacular.com/recipeImages/601216-556x370.jpg')
+  });
+
+  it('Should have a tag', () => {
+    expect(recipe.tags).to.deep.equal(['side dish']);
+  });
+
+  it('Should hold its own ingredient data', () => {
+    expect(recipe.ingredients).to.equal(recipeData[47].ingredients);
+  });
+
+  it('Should hold its own instruction data', () => {
+    expect(recipe.instructions).to.equal(recipeData[47].instructions);
+  });
 
   it('Should be able to calculate the cost of its ingredients', () => {
-    // console.log(ingredientsData);
     expect(recipe.calculateCost()).to.equal(4166);
   });
 
-
 });
+
+// describe('Recipe Data', () => {
+
+  // it('Should hold its own ingredient data', () => {
+  //   expect(recipe.ingredients).to.equal(recipeData[47].ingredients);
+  // })
+
+  // it('Should hold its own instruction data', () => {
+  //   expect(recipe.instructions).to.equal(recipeData[47].instructions);
+  // })
+// })
+
+// it('Should be able to calculate the cost of its ingredients', () => {
+//   // console.log(ingredientsData);
+//   console.log(recipe.calculateCost());
+//   expect(recipe.calculateCost()).to.equal(4166);
+// });
+
+
+// });
