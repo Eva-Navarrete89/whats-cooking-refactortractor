a4632008bf4f9335a97ea61bb41272d954be3645
Em Orendorff <em.orendorff@gmail.com>
2021-06-06 13:50:45 -0500

    Add tests to Pantry test and create constructor for class

--- src/pantry.js
+++ src/pantry.js
@@ -2,6 +2,18 @@ class Pantry {
   constructor(userIngredients) {
     this.contents = userIngredients;
   }
+
+  findIngredientsNecessary(recipe) {
+    let ingredientsNeeded = {};
+    recipe.ingredients.forEach(ingredient => {
+      ingredientsNeeded[ingredient.id] = ingredient.quantity.amount;
+    })
+    return ingredientsNeeded;
+  }
+
+  checkCanMakeRecipe(recipe) {
+    let
+  }
 }
 
 export default Pantry;
--- test/pantry-test.js
+++ test/pantry-test.js
@@ -8,11 +8,27 @@ const recipes = require('../data/recipes');
 describe('Pantry', () => {
     let pantry;
 
-    beforeEach(function() {
+    beforeEach('Pantry', () => {
         pantry = new Pantry(users[0].pantry)
     })
 
     it('should be a function', () => {
         expect(Pantry).to.be.a('function');
+    })
+
+    it('should create an instance of Pantry', () => {
+        expect(pantry).to.be.an.instanceOf(Pantry);
+    })
+
+    it('should have an array of user ingredients', function() => {
+        expect(pantry.contents[0].ingredient).to.equal(11477);
+    })
+
+    it('should check if user has enough ingredients for a recipe', () => {
+        let sampleRecipe = recipes[0];
+        let canMake = pantry.checkCanCookRecipe(sampleRecipe);
+        let failResponse = "Sorry, you don't have all the ingredients needed!"
+
+        expect(canMake).to.equal(failResponse);
     })
 })
